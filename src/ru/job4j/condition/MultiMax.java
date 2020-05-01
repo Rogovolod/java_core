018947c72fa283be9bec2121476db212d2ab1910
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-01 22:53:11 +1000

    MultiMax

---
+++ src/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public int max(int first, int second, int third) {
+        int rsl = ((first < second) ? second : first);
+        int result = ((rsl > third) ? rsl : third);
+        return result;
+    }
+}
