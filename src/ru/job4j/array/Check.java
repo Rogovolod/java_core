53a749fbaa355c9161f757455dfa9a2112fbb292
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-05 17:34:52 +1000

    Check

---
+++ src/ru/job4j/array/Check.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+       for (int index = 0; index < data.length - 1; index++) {
+           if (data[data.length - 1] != data[data.length - index - 1]) {
+               result = false;
+           }
+       }
+        return result;
+    }
+}
