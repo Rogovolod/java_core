fa9d81cf62ac684651ffdc06bbfcb2c635c254fe
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-06 18:30:38 +1000

    BarleyBreak

---
+++ src/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int[][] numbers = new int[3][3];
+        numbers[0][0] = 1;
+        numbers[0][1] = 2;
+        numbers[0][2] = 3;
+        numbers[1][0] = 4;
+        numbers[1][1] = 5;
+        numbers[1][2] = 6;
+        numbers[2][0] = 7;
+        numbers[2][1] = 8;
+        numbers[2][2] = 9;
+    }
+}
