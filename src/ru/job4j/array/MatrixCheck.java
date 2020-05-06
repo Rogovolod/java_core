d2bf9173d178bcc2f682f6ad27f581a55b99a7a7
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-06 21:10:00 +1000

    MatrixCheck

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -3,14 +3,23 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = 1; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[row][j] != board[row][i]) {
+        for (int i = 0; i < board.length; i++) {
+                if (board[row][0] != board[row][i]) {
                     result = false;
                     break;
-                }
             }
         }
         return result;
-        }
     }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+                if (board[0][column] != board[i][column]) {
+                    result = false;
+                    break;
+            }
+        }
+        return result;
+    }
+}
