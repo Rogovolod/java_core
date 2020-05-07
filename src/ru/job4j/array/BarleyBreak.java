d9f0f06eee6eb2c6eccbeb9a96f103b28dd8d063
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-07 20:28:51 +1000

    BarleyBreak

--- src/ru/job4j/array/BarleyBreak.java
+++ src/ru/job4j/array/BarleyBreak.java
@@ -2,15 +2,14 @@ package ru.job4j.array;
 
 public class BarleyBreak {
     public static void main(String[] args) {
-        int[][] numbers = new int[3][3];
-        numbers[0][0] = 1;
-        numbers[0][1] = 2;
-        numbers[0][2] = 3;
-        numbers[1][0] = 4;
-        numbers[1][1] = 5;
-        numbers[1][2] = 6;
-        numbers[2][0] = 7;
-        numbers[2][1] = 8;
-        numbers[2][2] = 9;
-    }
-}
+        int[][] num = new int[3][3];
+            for (int i = 0; i < num.length; i++) {
+                for (int j = 0; j < num[i].length; j++) {
+                    num[i][j] = j + 1 + i * num.length;
+                    System.out.print(num[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+       }
+
