f86e2cf889c492e4a01afb8ed65cb92825e6268d
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-14 20:56:39 +1000

    DummyDic

--- src/ru/job4j/oop/DummyDic.java
+++ src/ru/job4j/oop/DummyDic.java
@@ -3,11 +3,7 @@ package ru.job4j.oop;
 public class DummyDic {
 
     public String engToRus(String eng) {
+        System.out.println("Неизвестное слово. " + eng);
         return eng;
-    }
-    public static void main(String[] args) {
-        DummyDic rus = new DummyDic();
-        String translate = rus.engToRus("blablabla");
-        System.out.println("Неизвестное слово. " + translate);
     }
 }
