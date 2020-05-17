47cae5154d081d5f5918e8fa4c96e5ebee3bb6be
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-17 13:41:38 +1000

    Triangle

--- src/ru/job4j/condition/Triangle.java
+++ src/ru/job4j/condition/Triangle.java
@@ -2,7 +2,30 @@ package ru.job4j.condition;
 
 public class Triangle {
 
-    public static boolean exist(double ab, double ac, double bc) {
-        return ab + ac > bc && ac + bc > ab && ab + bc > ac ? true : false;
+    public double period(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return p;
+    }
+
+    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+        Point first = new Point(x1, y1);
+        Point second = new Point(x2, y2);
+        Point third = new Point(x3, y3);
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+        double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        if (this.exist(a, b, c)) {
+            rsl = -1;
+        }
+        return rsl;
+    }
+
+    private boolean exist(double a, double c, double b) {
+        if ((a + b) < c || (a + c) < b || (b + c) < a) {
+            return false;
+        }
+        return true;
     }
 }
