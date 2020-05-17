65c7cc976c1089a7e2f83a7c375eb31963c0e071
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-17 13:04:37 +1000

    Point

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -1,19 +1,25 @@
 package ru.job4j.condition;
 
-import java.math.MathContext;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double first = x2 - x1;
-        double second = y2 - y1;
-        double a = Math.pow(first, 2);
-        double b = Math.pow(second, 2);
-        double plus = a + b;
-        double rsl = Math.sqrt(plus);
-        return rsl;
+
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
+    public  double distance(Point that) {
+        return  sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
     public static void main(String[] args) {
-        double result = Point.distance(3, 0, 0, 3);
-        System.out.println("result (3, 0) to (0, 3) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
