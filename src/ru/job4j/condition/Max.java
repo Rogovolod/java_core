d0d481c640c643af91ac7323221c87791245162d
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-17 15:46:47 +1000

    Max

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,7 +2,31 @@ package ru.job4j.condition;
 
 public class Max {
 
-    public static int max(int left, int right) {
+    public static int max(int first, int second, int third) {
+        int rsl = 0;
+        if (first >= second && first >= third) {
+            rsl = first;
+        }
+        if (second >= first && second >= third) {
+           rsl = second;
+        }
+        if (third >= first && third >= second) {
+            rsl = third;
+        }
+        return rsl;
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(
+                first, second,
+                max(second, third, fourth)
+        );
+    }
+
+/**
+* This old decision when max from 2
+ * */
+    public static int max2(int left, int right) {
         boolean condition = left > right;
         int result = condition ? left : right;
         return result;
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -5,19 +5,55 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class MaxTest {
+
     @Test
     public void whenMax1To2Then2() {
-        int result = Max.max(1, 2);
+        int result = Max.max2(1, 2);
         assertThat(result, is(2));
     }
     @Test
     public void whenMax2To1Then2() {
-        int result = Max.max(2, 1);
+        int result = Max.max2(2, 1);
         assertThat(result, is(2));
     }
     @Test
     public void whenMax2To2Then2() {
-        int result = Max.max(2, 2);
+        int result = Max.max2(2, 2);
         assertThat(result, is(2));
+    }
+    @Test
+    public void whenMax3To1Then3() {
+        int result = Max.max(1, 2, 3);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenMax2To1Then3() {
+        int result = Max.max(2, 3, 1);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void whenMax2to2Equal() {
+        int result = Max.max(2, 2, 2);
+        assertThat(result, is(2));
+    }
+    @Test
+    public void whenMax1To4Then4() {
+        int result = Max.max(1, 2, 3, 4);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenMax4To1Then4() {
+        int result = Max.max(4, 3, 2, 1);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenMax7Equal() {
+        int result = Max.max(7, 7, 7, 7);
+        assertThat(result, is(7));
+    }
+    @Test
+    public void whenMax3To5Then9() {
+        int result = Max.max(3, 9, 9, 5);
+        assertThat(result, is(9));
     }
 }
