cd2265d04a8e554180ffdd199e7927e910cc3018
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-19 20:26:00 +1000

    PointTest

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -12,6 +12,6 @@ public class PointTest {
         Point one = new Point(1, 0, 0);
         Point two = new Point(0, 5, 4);
         double result = one.distance3d(two);
-        assertThat(result, is(6.48074069840786));
+        Assert.assertEquals(result, 6.48,  0.01);
     }
 }
