5d1615e2f8979f7d9f58783e83bb06d81d14c267
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-23 11:41:24 +1000

    Animal

--- src/ru/job4j/oop/Animal.java
+++ src/ru/job4j/oop/Animal.java
@@ -3,14 +3,14 @@ package ru.job4j.oop;
 public class Animal {
 
     public Animal() {
+    }
+
+    public Animal(String name) {
         super();
         System.out.println("load animal");
     }
 
-    public Animal(String name) {
-    }
-
     public static void main(String[] args) {
-        Predator predator = new Predator("predator");
+        Animal animal = new Animal();
     }
 }
--- src/ru/job4j/oop/Predator.java
+++ src/ru/job4j/oop/Predator.java
@@ -1,13 +1,16 @@
 package ru.job4j.oop;
 
     public class Predator extends Animal {
+
+       public Predator() {
+       }
+
         public Predator(String name) {
             super(name);
             System.out.println("load predator");
         }
 
-
         public static void main(String[] args) {
-            Tiger tiger = new Tiger("tiger");
+            Predator predator = new Predator();
         }
     }
