7421c523560303b31e8e15baef170180d8127951
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-23 14:33:09 +1000

    college

---
+++ src/ru/job4j/pojo/Student.java
@@ -0,0 +1,33 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String fio;
+    private byte group;
+    private String start;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public byte getGroup() {
+        return group;
+    }
+
+    public void setGroup(byte group) {
+        this.group = group;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+}
