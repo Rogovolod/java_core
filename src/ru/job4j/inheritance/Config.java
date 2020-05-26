0bb5df736a86ccaee0c7862d2438bc13da9522b6
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-05-26 21:39:16 +1000

    Config

--- src/ru/job4j/inheritance/Config.java
+++ src/ru/job4j/inheritance/Config.java
@@ -13,11 +13,11 @@ public class Config {
         System.out.println(position);
     }
 
-    public String getProperty(String key) {
+    private String getProperty(String key) {
         return search(key);
     }
 
-    private String search(String key) {
+    public String search(String key) {
         return key;
     }
 }
