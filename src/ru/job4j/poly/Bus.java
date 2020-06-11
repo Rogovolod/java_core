7730e8480ce94f2ccf2af98ad09d50bdc5f5ba68
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-06-11 22:20:47 +1000

    bus

--- src/ru/job4j/poly/Bus.java
+++ src/ru/job4j/poly/Bus.java
@@ -3,13 +3,11 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public String drive(String question) {
-        return null;
     }
 
     @Override
     public int passenger(String question) {
         int psngsNmbr = 0;
-        return 0;
     }
 
     @Override
