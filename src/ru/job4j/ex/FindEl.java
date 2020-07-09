7e118032e0a5120a3c549b56478f62b03c544d99
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-07-09 19:35:53 +1000

    indexOf

--- src/ru/job4j/ex/FindEl.java
+++ src/ru/job4j/ex/FindEl.java
@@ -5,6 +5,7 @@ public class FindEl {
         int rsl = -1;
         for (int i = 0; i < value.length; i++) {
             if (key.equals(value[i])) {
+                rsl = i;
                 break;
             }
         }
