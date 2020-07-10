2c36496fee3e7dc49bc25399e31d5ce594f6abec
Rogovolod <savin.pavel.pavlovich@gmail.com>
2020-07-10 19:09:45 +1000

    Exception

--- src/ru/job4j/ex/UserStore.java
+++ src/ru/job4j/ex/UserStore.java
@@ -5,25 +5,30 @@ public class UserStore {
     public static User findUser(User[] users, String login) throws UserNotFoundException {
         int rsl = -1;
         for (int i = 0; i < users.length; i++) {
-            if (login.equals(users[i])) {
+            if (login.equals(User.getUsername())) {
                 rsl = i;
                 break;
             }
-            if (rsl == -1) {
-                throw new UserNotFoundException("User not found.");
-            }
+        }
+        if (rsl == -1) {
+            throw new UserNotFoundException("User not found.");
         }
         return null;
     }
 
     public static boolean validate(User user) throws UserInvalidException {
+        boolean rsl = false;
         if (user.isValid() == false) {
             throw new UserInvalidException("User is not validate");
-            if (user(String.length(username)) < 3) {
-                throw new UserInvalidException("Name is too short.")
-            }
+        } else {
+            rsl = true;
         }
-        return false;
+        if (user.getUsername().length < 3) {
+                throw new UserInvalidException("Name is too short.");
+            } else {
+            rsl = true;
+        }
+        return rsl;
     }
 
     public static void main(String[] args) {
@@ -35,8 +40,7 @@ public class UserStore {
             if (validate(user)) {
                 System.out.println("This user has an access");
             }
-        }
-        catch (UserInvalidException ui) {
+        } catch (UserInvalidException ui) {
             ui.printStackTrace();
         } catch (UserNotFoundException en) {
             en.printStackTrace();
