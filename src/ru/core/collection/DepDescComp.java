6052bba629999a1be4f2bba21d26dfb08ed3e925
Rogovolod <Rogovolod@gmail.com>
2021-04-10 00:16:28 +0900

    Отсортировать департаменты [#280619] fix 2

--- src/ru/core/collection/DepDescComp.java
+++ src/ru/core/collection/DepDescComp.java
@@ -5,11 +5,10 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        String[] a1 = o1.split("/");
-        String[] a2 = o2.split("/");
-        if (a2[0].compareTo(a1[0]) == 0) {
+        int rsl = o2.split("/")[0].compareTo(o1.split("/")[0]);
+        if (rsl == 0) {
             return o1.compareTo(o2);
         }
-        return a2[0].compareTo(a1[0]);
+        return rsl;
     }
 }
