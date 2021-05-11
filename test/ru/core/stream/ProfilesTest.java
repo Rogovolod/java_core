7996d804f7f5a31bbb6e1c21215fac05456be351
Rogovolod <Rogovolod@gmail.com>
2021-05-11 15:34:00 +0900

    3. Уникальность элементов и сортировка. [#280494] FixTests

--- test/ru/core/stream/ProfilesTest.java
+++ test/ru/core/stream/ProfilesTest.java
@@ -13,11 +13,15 @@ public class ProfilesTest {
         var one = new Address("St.Peterburg","Pushkina", 15, 15);
         var two = new Address("Moskow","Lomonosova", 35, 256);
         var three = new Address("Oslo","Gyermunbu", 12, 0);
+        var four = new Address("Oslo","Gyermunbu", 12, 0);
+        var five = new Address("Moskow","Lomonosova", 35, 256);
 
         var rsl = new ArrayList<Profile>();
         rsl.add(new Profile(one));
         rsl.add(new Profile(two));
         rsl.add(new Profile(three));
+        rsl.add(new Profile(four));
+        rsl.add(new Profile(five));
 
         var exp = new ArrayList<Address>();
         exp.add(two);
