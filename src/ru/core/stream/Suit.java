73431f9258596e589da05da33427d83a4dea6c1e
Rogovolod <Rogovolod@gmail.com>
2021-05-15 14:25:33 +0900

    8. Перенос методов в Stream API. [#280499]

---
+++ src/ru/core/stream/Suit.java
@@ -0,0 +1,5 @@
+package ru.core.stream;
+
+public enum Suit {
+    Diamonds, Hearts, Spades, Clubs
+}
---
+++ src/ru/core/stream/Value.java
@@ -0,0 +1,5 @@
+package ru.core.stream;
+
+public enum Value {
+    V_6, V_7, V_8, V_9, V_10
+}
