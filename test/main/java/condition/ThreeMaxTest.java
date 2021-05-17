081923a7397276d7a6751582ae5b965277d1997c
Rogovolod <Rogovolod@gmail.com>
2021-05-17 13:45:51 +0900

    refactor dir tests

--- test/main/java/condition/ThreeMaxTest.java
+++ test/main/java/condition/ThreeMaxTest.java
@@ -1,9 +1,9 @@
 package condition;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class ThreeMaxTest {
 
