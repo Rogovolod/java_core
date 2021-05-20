c1e6383d1e59f598ac12a3be47fbf933ef0e01c7
Rogovolod <Rogovolod@gmail.com>
2021-05-20 10:50:12 +0900

    1.1. Что такое import [#504773]

---
+++ src/main/java/calculator/MathCalculator.java
@@ -0,0 +1,28 @@
+package calculator;
+
+import static calculator.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+    public static double minusAndDivSum(double first, double second) {
+        return minus(first, second)
+                + div(first, second);
+    }
+
+    public static double allFunctionsSum(double first, double second) {
+        return minus(first, second)
+                + div(first, second)
+                + sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + minusAndDivSum(10, 20));
+        System.out.println("Результат расчета равен: " + allFunctionsSum(10, 20));
+    }
+}
