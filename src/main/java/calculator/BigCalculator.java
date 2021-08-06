02da90616fb3cb8e738dc84086901b32f1467d59
p.savin <pavel.savin.pavlovich@gmail.com>
2021-08-06 15:19:27 +0900

    Big Decimal calculator

--- src/main/java/calculator/BigCalculator.java
+++ src/main/java/calculator/BigCalculator.java
@@ -1,24 +1,45 @@
 package calculator;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class BigCalculator {
 
     public static void main(String[] args) {
         var in = new Scanner(System.in);
-        var first = in.nextBigDecimal();
-        var sign = in.next();
-        var second = in.nextBigDecimal();
-        var rsl = new BigDecimal(0);
-        switch (sign) {
-            case "/" -> rsl = first.divide(second);
-            case "*" -> rsl = first.multiply(second);
-            case "+" -> rsl = first.add(second);
-            case "-" -> rsl = first.subtract(second);
+        while (true) {
+            var line = in.nextLine();
+            if (line.isBlank()) {
+                System.out.println("Your input is empty.");
+                continue;
+            }
+            if (line.contains("exit")) System.exit(-1);
+            line = line.replace("*", " * ");
+            line = line.replace("-", " - ");
+            line = line.replace("+", " + ");
+            line = line.replace("/", " / ");
+            var out = new Scanner(line);
+            try {
+                var first = out.nextBigDecimal();
+                var sign = out.next();
+                var second = out.nextBigDecimal();
+                var rsl = new BigDecimal(0);
+                switch (sign) {
+                    case "/" -> rsl = first.divide(second, 100, RoundingMode.HALF_UP);
+                    case "*" -> rsl = first.multiply(second);
+                    case "+" -> rsl = first.add(second);
+                    case "-" -> rsl = first.subtract(second);
+                }
+                System.out.println(rsl);
+            } catch (InputMismatchException i) {
+                System.out.println("Wrong input, just write numbers, it's calculator.");
+            } catch (NoSuchElementException e) {
+                System.out.println("Wrong input, don't forget arithmetic sign and second number.");
+            }
+
         }
-
-        System.out.println(rsl);
     }
-
 }
