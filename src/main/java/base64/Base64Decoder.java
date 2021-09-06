102185184d50f197615bf8428751b7e6f8c875c6
p.savin <pavel.savin.pavlovich@gmail.com>
2021-09-06 15:40:36 +1000

    +

---
+++ src/main/java/base64/Base64Decoder.java
@@ -0,0 +1,26 @@
+package base64;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.Scanner;
+
+public class Base64Decoder {
+
+    public static void main(String[] args) throws IOException {
+
+        var fis = new FileInputStream("src/main/java/base64/filesForDecoding/Base64TextForDecoding.txt");
+        var base64Text = new Scanner(fis).nextLine();
+        fis.close();
+
+        byte[] decodedBytes = Base64.getDecoder().decode(base64Text);
+
+        var outputStream = new FileOutputStream("src/main/java/base64/decodedFiles/decoded");
+        outputStream.write(decodedBytes);
+        outputStream.close();
+
+        System.exit(-1);
+    }
+
+}
