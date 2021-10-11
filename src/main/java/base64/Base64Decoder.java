e7d68dfbba2d94c4e2bc9af3744a6f23e5cdabc6
p.savin <pavel.savin.pavlovich@gmail.com>
2021-10-11 22:19:02 +1000

    +

--- src/main/java/base64/Base64Decoder.java
+++ src/main/java/base64/Base64Decoder.java
@@ -10,16 +10,19 @@ public class Base64Decoder {
 
     public static void main(String[] args) throws IOException {
 
-        var fis = new FileInputStream("src/main/java/base64/filesForDecoding/Base64TextForDecoding.txt");
-        var base64Text = new Scanner(fis).nextLine();
-        fis.close();
+//        var fis = new FileInputStream("src/main/java/base64/filesForDecoding/Base64TextForDecoding.txt");
+//        var base64Text = new Scanner(fis).nextLine().split(",")[1];
+//        fis.close();
+//
+//        byte[] decodedBytes = Base64.getDecoder().decode(base64Text);
+//
+//        var outputStream = new FileOutputStream("src/main/java/base64/decodedFiles/decoded");
+//        outputStream.write(decodedBytes);
+//        outputStream.close();
 
-        byte[] decodedBytes = Base64.getDecoder().decode(base64Text);
-
-        var outputStream = new FileOutputStream("src/main/java/base64/decodedFiles/decoded");
-        outputStream.write(decodedBytes);
-        outputStream.close();
-
+        var a = "Юху!";
+        var b = Base64.getEncoder().encodeToString(a.getBytes());
+        System.out.println(b);
         System.exit(-1);
     }
 
