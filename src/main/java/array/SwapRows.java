2e85603de6944cfd725b9ec7a77afbe3ddab4fde
p.savin <pavel.savin.pavlovich@gmail.com>
2021-10-29 19:16:27 +1000

    +

---
+++ src/main/java/array/SwapRows.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Arrays;
+
+public class SwapRows {
+
+
+    public static void main(String[] args) {
+
+        int[][] test = new int[][] {
+                {4, 5, 6, 7, 8, 9},
+                {1, 2, 3, 4, 9, 9}
+        };
+        System.out.println(Arrays.deepToString(test));
+        swap(test, 0, 1);
+        System.out.println(Arrays.deepToString(test));
+
+    }
+
+
+    public static void swap(int[][] data, int src, int dst) {
+        for (int i = 0; i < data[src].length; i++) {
+            data[src][i] = data[src][i] + data[dst][i];
+            data[dst][i] = data[src][i] - data[dst][i];
+            data[src][i] = data[src][i] - data[dst][i];
+        }
+    }
+}
