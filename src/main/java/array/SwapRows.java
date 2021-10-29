4c18ab46665953d54afd588b07b35f2ec33d7bec
p.savin <pavel.savin.pavlovich@gmail.com>
2021-10-29 19:41:50 +1000

    +

--- src/main/java/array/SwapRows.java
+++ src/main/java/array/SwapRows.java
@@ -17,7 +17,6 @@ public class SwapRows {
 
     }
 
-
     public static void swap(int[][] data, int src, int dst) {
         for (int i = 0; i < data[src].length; i++) {
             data[src][i] = data[src][i] + data[dst][i];
