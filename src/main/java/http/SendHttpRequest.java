e5e561564575bf51c1975d8dbfd46fd54a08fa5b
Rogovolod <rogovolod@gmail.com>
2022-02-28 00:33:34 +1000

    http

---
+++ src/main/java/http/SendHttpRequest.java
@@ -0,0 +1,47 @@
+package http;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class SendHttpRequest {
+
+    public static void createRequest() throws IOException {
+        URL url = new URL("https://javarush.ru/api/1.0/rest/me");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setInstanceFollowRedirects(false);
+
+        int status = connection.getResponseCode();
+/*        if (status == HttpURLConnection.HTTP_MOVED_TEMP
+                || status == HttpURLConnection.HTTP_MOVED_PERM) {
+            String location = connection.getHeaderField("Location");
+            URL newUrl = new URL(location);
+            connection = (HttpURLConnection) newUrl.openConnection();
+        }*/
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer content = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        connection.disconnect();
+        System.out.println(status);
+//        System.out.println(content);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 50; i++) {
+            createRequest();
+        }
+        createRequest();
+
+    }
+
+}
