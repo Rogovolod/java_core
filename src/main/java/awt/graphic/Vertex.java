2f39b499a8cbc243f5244f65b5c46cfb3558d5ac
Rogovolod <rogovolod@gmail.com>
2022-06-28 01:53:46 +1000

    3D model of tetrahedron

---
+++ src/main/java/awt/graphic/Vertex.java
@@ -0,0 +1,14 @@
+package awt.graphic;
+
+class Vertex {
+
+    double x;
+    double y;
+    double z;
+
+    Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
